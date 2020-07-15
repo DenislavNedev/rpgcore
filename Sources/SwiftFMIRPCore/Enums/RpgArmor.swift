c2370a4cc75fd07a672449305fba55306870f055
Denislav Nedev <dnedev98@gmail.com>
2020-07-15 05:40:11 +0000

    Created new enum - RpgArmor
    1. Changed the version of swift to 5.0 since replit is working with 5.0 not 5.2
    2. Created .replit config file
    3. Created RpgArmor enum with all the armors which will be used in the rpg game

---
+++ Sources/SwiftFMIRPCore/Enums/RpgArmor.swift
@@ -0,0 +1,65 @@
+enum RpgArmor{
+  case noArmor
+  case leatherArmor
+  case chainShirt
+  case mediumHide
+  case bandedMail
+  case scaleMail
+  case halfPlate
+  case fullPlate
+  case bucklerShield
+  case heavyShield
+  case lightShield
+}
+
+extension RpgArmor : Armor  {
+    var defence : Int {
+      switch self {
+        case .noArmor: return  0
+        case .leatherArmor: return  3
+        case .chainShirt: return  3
+        case .mediumHide: return  5
+        case .bandedMail: return  7
+        case .scaleMail: return  6
+        case .halfPlate: return  5
+        case .fullPlate: return  10
+        case .bucklerShield: return  7
+        case .heavyShield: return  13
+        case .lightShield: return  9 
+      }
+  }
+
+    var attack : Int {
+      switch self {
+        case .noArmor: return  0
+        case .leatherArmor: return  1
+        case .chainShirt: return  2
+        case .mediumHide: return  1
+        case .bandedMail: return  2
+        case .scaleMail: return  1
+        case .halfPlate: return  1
+        case .fullPlate: return  2
+        case .bucklerShield: return  2
+        case .heavyShield: return  3
+        case .lightShield: return  2 
+      }
+  }
+}
+
+extension RpgArmor : CustomStringConvertible  {
+  var description: String {
+        switch self {
+          case .noArmor: return  "No Armor"
+          case .leatherArmor: return  "Leather Armor"
+          case .chainShirt: return  "Chain Shirt"
+          case .mediumHide: return  "Medium Hide"
+          case .bandedMail: return  "Banded Mail"
+          case .scaleMail: return  "Scale Mail"
+          case .halfPlate: return  "Half Plate"
+          case .fullPlate: return  "Full Plate"
+          case .bucklerShield: return  "Buckler Shield"
+          case .heavyShield: return  "Heavy Shield"
+          case .lightShield: return  "Light Shield"
+        }
+      }
+}
